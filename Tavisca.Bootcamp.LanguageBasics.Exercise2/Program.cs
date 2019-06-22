f0ecd88c1af736f218d3b11ab1526777addd5188
tavisca-rkaushik <51960863+tavisca-rkaushik@users.noreply.github.com>
2019-06-22 16:18:27 +0530

    Update Program.cs
    Added the changes required

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+using System;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -21,9 +20,57 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
+        public static bool isinvalid(string[] exactPostTime, string[] showPostTime)
+        {
+            for (int i = 0; i < exactPostTime.Length; i++) 
+            {
+                for (int j = i + 1; j < exactPostTime.Length; j++)
+                {
+                    if (exactPostTime[i] == exactPostTime[j])
+                    {
+                        if (showPostTime[i] != showPostTime[j])
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
+            //check for test case 3 where the case is invalid
+            if(isinvalid(exactPostTime,showPostTime))
+            {
+                return "impossible";
+            }
+
+            string[] Final = new string[exactPostTime.Length];
+            for(int i = 0; i<exactPostTime.Length; i++)
+            {
+                string[] split_hour_min_sec = exactPostTime[i].Split(":");
+                DateTime dateTime = new DateTime(1996, 7, 25, Int32.Parse(split_hour_min_sec[0]), Int32.Parse(split_hour_min_sec[1]), Int32.Parse(split_hour_min_sec[2]));
+                 //if seconds need to be added
+                 if(showPostTime[i].Contains("seconds")) 
+                 {
+                    Final[i] = exactPostTime[i];
+                }
+                //if minutes need to be added
+                else if(showPostTime[i].Contains("minutes"))
+                {
+                    string minutes = showPostTime[i].Split(" ")[0];      
+                    TimeSpan timeSpan = new TimeSpan(0, Int32.Parse(minutes), 0);  
+                    Final[i] = (dateTime.Add(timeSpan)).ToString().Split(" ")[1];
+                }
+                //if hours need to be added
+                else if(showPostTime[i].Contains("hours"))
+                {
+                    string hours = showPostTime[i].Split(" ")[0];
+                    TimeSpan timeSpan = new TimeSpan(Int32.Parse(hours), 0, 0);
+                    Final[i] = dateTime.Add(timeSpan).ToString().Split(" ")[1];
+                }
+            }
+            Array.Sort(Final);
+            return Final[(exactPostTime.Length-1)];
             throw new NotImplementedException();
         }
     }
